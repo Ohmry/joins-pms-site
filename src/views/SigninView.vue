78201556f8a53bde9246aae33209bb82133d9a33
Ohmry <o.ohmry@gmail.com>
2022-07-13 21:11:23 +0900

    2022.07.13

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -11,6 +11,9 @@
       <h4>비밀번호</h4>
       <input type="text" />
     </section>
+    <section class="btnbox">
+      <button class="signin">로그인</button>
+    </section>
   </main>
 </template>
 
@@ -20,26 +23,48 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 main {
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 100vh;
-}
-main > section {
-  align-self: center;
-}
-main > section > h4 {
-  display: inline-block;
-  width: 80px;
-  margin: 5px;
-  padding: 0 10px 0 0;
-  text-align: right;
+
+  section {
+    align-self: center;
+
+    h4 {
+      display: inline-block;
+      width: 70px;
+      margin: 5px 0;
+      padding: 0 10px 0 0;
+      text-align: right;
+    }
+    input[type=text] {
+      width: 212px;
+    }
+  }
 }
 .logo {
   text-align: center;
   font-size: 50px;
-  color: white;
+  color: var(--primary-color);
+}
+
+section.btnbox {
+  padding: 15px 0 0 0;
+  button.signin {
+    width: 300px;
+    background-color: var(--primary-color);
+    border: 1px solid var(--primary-color);
+    border-radius: 5px;
+    padding: 10px 0;
+    color: white;
+    cursor: pointer;
+
+    &:active {
+      background-color: green;
+    }
+  }
 }
 </style>
