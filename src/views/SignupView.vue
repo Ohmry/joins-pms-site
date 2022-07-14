1c7a988fc0071b6ca56e224e2700ffc619e8d39a
Ohmry <o.ohmry@gmail.com>
2022-07-15 08:18:02 +0900

    2022.07.15.1

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,12 +3,63 @@
     <router-link to="/">
       <font-awesome-icon icon="fa-solid fa-fan"></font-awesome-icon>
     </router-link>
+    <section>
+      <h3>사용자 등록</h3>
+    </section>
+    <section>
+      <span>이메일</span>
+      <input type="email" v-model="user.email" />
+    </section>
+    <section>
+      <span>비밀번호</span>
+      <input type="password" v-model="user.password" />
+    </section>
+    <section>
+      <span>이름</span>
+      <input type="name" v-model="user.name" />
+    </section>
+    <section class="btnbox">
+      <button @click="test">등록</button>
+    </section>
   </main>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data: () => {
+    return {
+      user: {
+        email: '',
+        password: '',
+        name: ''
+      }
+    }
+  },
+  methods: {
+    test: function () {
+      axios({
+        method: 'post',
+        url: '/api/test',
+        baseURL: 'http://localhost:9091',
+        data: JSON.stringify({ message: 'Hello World' }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => console.log(response)).catch(err => console.error(err))
+    },
+    signup: function () {
+      axios({
+        method: 'post',
+        url: '/api/signup',
+        baseURL: 'http://localhost:9091/',
+        data: JSON.stringify(this.user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => console.log(response)).catch(err => console.error(err))
+    }
+  }
 }
 </script>
 
